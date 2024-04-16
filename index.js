3db599e1661371aaa1cdc166e471d48fa2f5c8f6
Muhammad Ahmed <muhammadahmad742@gmail.com>
2024-04-16 18:55:22 +0500

    json data

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
 
+const employee_data = {
+  employee: {
+    name: "sonoo",
+    salary: 56000,
+    married: true,
+  },
+};
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.get("/twitter", (req, res) => {
   res.send("Drdragon");
 });
+app.get("/empdata", (req, res) => {
+  res.json(employee_data);
+});
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port `);
 });
